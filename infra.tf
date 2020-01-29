8c615d3fb8c569e0a9b3719e1c3d7be2716cc50c
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-01-29 11:47:56 +0100

    .travis.yml init.

---
+++ infra.tf
@@ -0,0 +1,53 @@
+provider "aws" {
+    profile    = "default"
+    region     = "eu-west-3"
+}
+
+resource "aws_key_pair" "heticmadu_key_pair" {
+    key_name   = "heticmadu"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDlCOqXqzZXOf39Fou2TgQ0CVFODUkSiG2iMh9hbME/yiKWiqE+5lwGD1t7WG724m5qY6SKqdltUCnuUeJFXC2R6l1yBuSCMTb5CxQaf4l4BBs/tcSZGcMO4affqMq31LoxRFW4aA5UU3jS14whZeXM1z9BztQ2Pwllp9RPxGjfKfXd1ge8pzW1wluu7ISAiZQxwNNg//MIBazrxBxw+PxtT/3eH3QF0Js/q4tV1cGy1U39kNH/pF3nJAmxWED5sEEjZNxMeI28sl0pP0V0kf5lIqJQjKtVhoI1++KdPoJPkbNDEmBlAFXxdak6DYW5eFp68XOsUkWP+Sh/38XhjPYV heticmadu@gmail.com"
+}
+
+resource "aws_instance" "heticmadu_instance" {
+    ami           = "ami-087855b6c8b59a9e4"
+    instance_type = "t2.micro"
+    key_name      = "heticmadu"
+}
+
+resource "aws_default_vpc" "heticmadu_default_vpc" {
+    tags = {
+        Name = "Default VPC"
+    }
+}
+
+resource "aws_security_group" "heticmadu_security_group" {
+    vpc_id          = "${aws_default_vpc.heticmadu.id}"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 3000
+        to_port     = 3000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 4000
+        to_port     = 4000
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = "-1"
+        cidr_blocks     = ["0.0.0.0/0"]
+    }
+}
