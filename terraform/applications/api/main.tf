d0566a14258938c26db9ddc46448e4683c1513a9
PierreTurnbull <pierre.turnbull42@gmail.com>
2020-05-14 13:19:02 +0200

    Added api elb.

--- terraform/applications/api/main.tf
+++ terraform/applications/api/main.tf
@@ -50,3 +50,33 @@ resource "aws_security_group" "api" {
 output "authorized_security_group_id" {
   value = aws_security_group.api.id
 }
+
+resource "aws_elb" "api" {
+  name               = "heticmadu-${var.stage}-api"
+  availability_zones = ["eu-west-3a"]
+
+  listener {
+    instance_port     = 4000
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:4000/api"
+    interval            = 30
+  }
+
+  instances                   = aws_instance.api.*.id
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "heticmadu-${var.stage}-api"
+  }
+}
